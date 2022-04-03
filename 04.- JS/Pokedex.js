cd4cf9e22f9a1731a220cf3c0e4f14d7b2c06298
Alonso Ismael Camarena <71803835+IsmaelCamna@users.noreply.github.com>
2022-04-02 20:47:51 -0600

    Update Pokedex.js

--- 04.- JS/Pokedex.js
+++ 04.- JS/Pokedex.js
@@ -1,69 +1,89 @@
 const fetchPokemon = () => {
     const pokeNameInput = document.getElementById("pokeName");
-    let pokeInput = pokeName.value.toLowerCase();
-    const buscarPokemon = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const buscarPokemon = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
     fetch(buscarPokemon).then((res) => {
         if (res.status != "200") {
             console.log(res);
-            pokeImage("C:\Users\Alonso\OneDrive\Desktop\Innovacion Virtual\FrontEnd\4.- JavaScript\img\NotFound.png")
+            pokeImage("./img/NotFound.png")
         }
         else {
             return res.json();
         }
     }).then((data) => {
         if (data) {
-            let pokeNam = data.name;
-            pokeNombre(pokeNam);
-            console.log(pokeNam);
+            let pokeId = data.id;
+            pokeNum(pokeId);
+            console.log(pokeId);
 
-            let pokeID = data.id;
-            pokeNum(pokeID);
-            console.log(pokeID);
+            let pokeNam = data.name;
+            pokeNombre(pokeNam.charAt(0).toUpperCase() + pokeNam.slice(1));
+            console.log(pokeNam.charAt(0).toUpperCase() + pokeNam.slice(1));
+
+            let pokeH = data.height;
+            pokePeso(pokeH);
+            console.log(pokeH);
+
+            let pokeW = data.weight;
+            pokeAltura(pokeW);
+            console.log(pokeW);
 
             let pokeImg = data.sprites.front_default;
             pokeImage(pokeImg);
             console.log(pokeImg);
 
-            let pokeStats = data.stats;
-            pokeEst(pokeStats);
-            console.log(pokeStats);
-
-            let pokeTypes = data.types;
+            let pokeTypes = data.types.map(tipo => tipo.type.name.charAt(0).toUpperCase() + tipo.type.name.slice(1));
             pokeTipo(pokeTypes);
             console.log(pokeTypes);
 
-            let pokeMoves = data.moves;
+            let pokeStats = data.stats.map(est => est.stat.name.toUpperCase() + " = " + est.base_stat);
+            pokeEst(pokeStats);
+            console.log(pokeStats);
+
+            let pokeMoves = data.moves.map(mov => mov.move.name.toUpperCase());
             pokeMove(pokeMoves);
             console.log(pokeMoves);
         }
     });
 }
-const pokeNombre = (buscarPokemon) => {
-    const pokeNombre = document.createElement('pokeNam');
-    pokeNombre.textContent = buscarPokemon.name; 
+
+const pokeNum = (pokeId) => {
+    Numero = document.getElementById("pokeId");
+    Numero.textContent = `N.° ${pokeId}`;
 }
 
-const pokeNum = (buscarPokemon) => {
-    const pokeNumero = document.createElement('pokeID');
-    pokeNumero.textContent = buscarPokemon.id; 
+const pokeNombre = (pokeNam) => {
+    nombre = document.getElementById("pokeNam");
+    nombre.textContent = `${pokeNam}`;
+}
+
+const pokePeso = (pokeH) => {
+    Peso = document.getElementById("pokeH");
+    Peso.textContent = `Height: ${pokeH/10} m`;
+}
+
+const pokeAltura = (pokeW) => {
+    Altura = document.getElementById("pokeW");
+    Altura.textContent = `Weight: ${pokeW/10} kg`;    
 }
 
 const pokeImage = (buscarPokemon) => {
     const pokePhoto = document.getElementById("pokeImg");
-    pokePhoto.src = buscarPokemon; 
+    pokePhoto.src = buscarPokemon;
 }
 
-const pokeEst = (buscarPokemon) => {
-    const pokeEstadisticas = document.createElement("pokeStats");
-    pokeEstadisticas.textContent = buscarPokemon.stats;
+const pokeTipo = (pokeTypes) => {
+    Tipo = document.getElementById("pokeTypes")
+    Tipo.textContent = `Type: ${pokeTypes}`;
 }
 
-const pokeTipo = (buscarPokemon) => {
-    const pokeTipo = document.createElement('pokeTypes');
-    pokeTipo.textContent = buscarPokemon.types; 
+const pokeEst = (pokeStats) => {
+    Estadisticas = document.getElementById("pokeStats");
+    Estadisticas.textContent = `${pokeStats}`;
 }
 
-const pokeMove = (buscarPokemon) => {
-    const pokeMovimientos = document.createElement('pokeMoves');
-    pokeMovimientos.textContent = buscarPokemon.moves; 
+const pokeMove = (pokeMoves) => {
+    Movimientos = document.getElementById("pokeMoves");
+    Movimientos.textContent = `${pokeMoves}`;
 }
